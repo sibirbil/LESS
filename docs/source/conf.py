0ea21445d2d451b3bc1a29ef6aac82e272d786a9
samet çopur <sametcopur@yahoo.com>
2025-11-22 16:56:01 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,14 +7,21 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 import os
 import sys
+from datetime import datetime
 sys.path.insert(0, os.path.abspath('../..'))
 
 from less import __version__
 
-project = 'LESS'
-copyright = '2025, Samet Çopur'
-author = 'Samet Çopur'
+
+# -- Project information -----------------------------------------------------
+
+project = "LESS"
+author = "İlker Birbil, Samet Çopur"
+current_year = datetime.now().year
+copyright = f"{current_year}, İlker Birbil, Samet Çopur"
 release = __version__
+
+
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
